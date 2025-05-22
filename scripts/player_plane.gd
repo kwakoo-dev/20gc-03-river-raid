57740ec7094462c2f78b8b15e8de645f86984749
kwakoo-dev <kwakoofficial@gmail.com>
2025-05-22 20:31:35 +0200

    #2 Create and animate a jet fighter - set animations for leaning left and right

--- scripts/player_plane.gd
+++ scripts/player_plane.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var _sprites = $Sprites
+
 const SPEED = 500.0
 
 @export var move_left : StringName
@@ -8,8 +10,11 @@ const SPEED = 500.0
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed(move_left):
 		velocity.x = -SPEED
+		_sprites.play("left")
 	elif Input.is_action_pressed(move_right):
 		velocity.x = SPEED
+		_sprites.play("right")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED) 
+		_sprites.play("default")
 	move_and_slide()
